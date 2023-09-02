0802ef82e34a8e8f4b7fe37aee15ea2806d801ad
SergioRon2 <rondonjr2005@gmail.com>
2023-09-02 15:36:15 -0500

    2/09/2023

--- app.py
+++ app.py
@@ -1,34 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 #  ruta original 
 
 @app.route('/')
+@app.route('/template.html')
 def ruta1():
     titulo = 'Actividad Back-End'
     return render_template('template.html', titulo=titulo)
 
 #  ruta notas 
 
-@app.route('/notas/<float:nota1>/<float:nota2>/<float:nota3>')
+@app.route('/notas.html/')
+@app.route('/notas.html/<float:nota1>/<float:nota2>/<float:nota3>')
 def notas(nota1,nota2,nota3):
-    resultado = (nota1*30)/100 + (nota2*30)/100 + (nota3*40)/100
-    return f'el promedio es: {resultado}'
-    return render_template('notas.html', resultado=resultado)
+    tituloNotas = 'Ingresa las notas en la url de esta forma: /notas/(nota1)/(nota2)/(nota3)'
+
+    proceso = (nota1*30)/100 + (nota2*30)/100 + (nota3*40)/100
+    resultado = f'el promedio es: {proceso}'
+    
+    return render_template('notas.html', resultado=resultado, tituloNotas = tituloNotas)
 
 #  esta es la ruta edad 
 
 @app.route('/edad.html/<int:a>')
 def edad(a):
     tituloEdad = 'Ingresa la edad en la url de esta forma: /edad/#'
-    if a < 18:
-        return f'<h1>La persona es menor de edad, tiene {a} años</h1>'
+    resultado = ''
+
+    # todo lo que esta comentado es el intento de introducir imagenes
+
+    if a == 0:
+        resultado = ''
+        # foto = ''
+    elif a < 18:
+        resultado = f'La persona es menor de edad, tiene {a} años'
+        # foto = url_for('static', filename='static/img/menores.jpg')
     elif a < 60:
-        return f'<h1>La persona es adulta, pues tiene {a} años</h1>'
+        resultado = f'La persona es adulta, pues tiene {a} años'
+        # foto = url_for('static', filename='static/img/adulto.jpg')
     else:
-        return f'<h1>La persona es adulta mayor, tiene {a} años</h1>'
-        return render_template('edad.html', tituloEdad=tituloEdad)
+        resultado = f'La persona es adulta mayor, tiene {a} años'
+        # foto = url_for('static', filename='static/img/adulto-mayor.jpg')
+
+    return render_template('edad.html', resultado=resultado, tituloEdad=tituloEdad)
+
+    # foto = foto <-- esto va dentro de render_template 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
